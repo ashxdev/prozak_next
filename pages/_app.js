7b62cc9645ad0acae225eb5289a6901c3da4d336
Andrii Shapovalov <ashapovalov@sharpminds.com>
2019-09-06 14:26:05 +0300

    add mobx store

--- pages/_app.js
+++ pages/_app.js
@@ -2,27 +2,51 @@ import React from "react"
 import App from "next/app"
 import Head from "next/head"
 
+import { Provider } from "mobx-react"
+import initializeStore from "../stores/stores"
+
 import globalStyles from "../static/style.js"
 
 import Layout from "../components/layouts/Layout"
 
 class ProzakApp extends App {
+  static async getInitialProps(appContext) {
+    const mobxStore = initializeStore()
+
+    appContext.ctx.mobxStore = mobxStore
+    const appProps = await App.getInitialProps(appContext)
+    return {
+      ...appProps,
+      initialMobxState: mobxStore,
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    const isServer = typeof window === "undefined"
+    this.mobxStore = isServer
+      ? props.initialMobxState
+      : initializeStore(props.initialMobxState)
+  }
+
   render() {
     const { Component, pageProps } = this.props
     return (
-      <Layout>
-        <Head>
-          <style amp-custom jsx global>
-            {globalStyles}
-          </style>
-          <script
-            async
-            custom-element="amp-carousel"
-            src="https://cdn.ampproject.org/v0/amp-carousel-0.2.js"
-          />
-        </Head>
-        <Component {...pageProps} />
-      </Layout>
+      <Provider {...this.mobxStore}>
+        <Layout>
+          <Head>
+            <style amp-custom jsx global>
+              {globalStyles}
+            </style>
+            <script
+              async
+              custom-element="amp-carousel"
+              src="https://cdn.ampproject.org/v0/amp-carousel-0.2.js"
+            />
+          </Head>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
     )
   }
 }
